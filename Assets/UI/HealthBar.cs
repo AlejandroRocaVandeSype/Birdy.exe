15f4e17ca144f6d9dfaf52b6edc4c7b15bbcbe67
Kruuuze <159920992+Kruuuze@users.noreply.github.com>
2024-02-13 23:04:43 +0100

    Added Corruption bar

---
+++ Assets/UI/HealthBar.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public Color fillCol;
+
+    // for testing, depending on how it is implemented in game may need to delete
+    void Start()
+    {
+        slider.maxValue = 10;
+        slider.value = 1;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+    
+    public void setMaxCorruption(int corruption)
+    {
+        slider.maxValue = corruption;
+        slider.value = 1;
+
+        fill.color = gradient.Evaluate(0f);
+    }
+
+    public void setCorruption(int corruption)
+    {
+        slider.value = corruption+1;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
